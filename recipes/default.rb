91ddd0980cff032dd2f8f12c51d37145fe7e2fe5
Kent Squires <ksquires@gmail.com>
2018-02-27 21:58:31 -0600

    added package upgrade resource back in

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,19 @@
 node.default['virtualbox']['version'] = '5.2'
 include_recipe 'virtualbox-install::default'
 
-# package 'VirtualBox-5.2' do
-#   action :upgrade
-# end
+package 'VirtualBox-5.2' do
+  notifies :run, 'execute[kill vms]', :before
+  action :upgrade
+end
+
+execute 'kill vms' do
+  command 'vboxmanage list runningvms | sed -r \'s/.*\{(.*)\}/\1/\' | xargs -L1 -I {} VBoxManage controlvm {} savestate'
+  action :nothing
+end
+
+execute 'vboxconfig' do
+  command '/sbin/vboxconfig'
+  user 'root'
+  action :nothing
+  subscribes :run, 'package[VirtualBox-5.2]', :immediately
+end

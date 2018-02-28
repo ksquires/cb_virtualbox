19eadf04fedbe3dfe387a2922c0a49d38c0f76ab
Kent Squires <ksquires@gmail.com>
2018-02-27 21:59:03 -0600

    added package upgrade resource back in

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures virtualbox'
 long_description 'Installs/Configures virtualbox'
-version '0.2.2'
+version '0.2.3'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'virtualbox-install'

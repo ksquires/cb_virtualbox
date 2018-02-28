260b4628ba205613c486a46504101e07aeab7ba2
Kent Squires <ksquires@gmail.com>
2018-02-27 20:50:41 -0600

    removed package install resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,6 @@
 node.default['virtualbox']['version'] = '5.2'
 include_recipe 'virtualbox-install::default'
 
-package 'VirtualBox-5.2' do
-  action :upgrade
-end
+# package 'VirtualBox-5.2' do
+#   action :upgrade
+# end
